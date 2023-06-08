ee1f3a93d7e08a4d69dc8250216362ab547f6b9f
victorprens <vprens12@gmail.com>
2023-06-07 19:23:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/agents')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Agents</h1>
+      {data && data.map(agent => (
+        <div key={agent.uuid}>
+          <h2>{agent.displayName}</h2>
+          <p>{agent.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
