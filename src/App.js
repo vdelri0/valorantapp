c884e65c77e67d39b0174ae4960ce521410ed0a3
victorprens <vprens12@gmail.com>
2023-06-14 19:42:56 -0500

    - Creates the card and image divs. - Creates the App.css style file for card styles.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import './App.css'; // Import CSS file for styling
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/api/agents')
-      .then(response => response.json())
-      .then(data => setData(data));
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/agents?is_playable=true', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log('Response Data:', responseData);
+          const dataArray = Object.values(responseData); // Convert the object to an array
+          console.log('Data Array:', dataArray);
+          setData(dataArray[1]);
+        } else {
+          throw new Error('Request failed with status ' + response.status);
+        }
+      } catch (error) {
+        console.error('Authorization failed:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div>
-      <h1>Agents</h1>
-      {data && data.map(agent => (
-        <div key={agent.uuid}>
-          <h2>{agent.displayName}</h2>
-          <p>{agent.description}</p>
-        </div>
-      ))}
+    <div className="App">
+      <header className="App-header">
+        <h1>Agents</h1>
+      </header>
+      <div className="card-container">
+        {data && data.map(agent => (
+          <div key={agent.uuid} className="card">
+            <img className="card-image" src={agent.bustPortrait} alt={agent.displayName} />
+            <div className="card-overlay">
+              <h2 className="card-name">{agent.displayName}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
